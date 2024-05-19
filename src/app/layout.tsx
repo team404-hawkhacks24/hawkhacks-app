4bf40e9c36c6e974547a8858e58954e80fababe1
Akshat Garg <gargaks@sheridancollege.ca>
2024-05-19 10:34:09 -0400

    Corrected spelling mistake

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({ children }: {
                 link: "/dashboard"
               },
               {
-                name: "Feeback",
+                name: "Feedback",
                 link: "/feedback"
               }
             ]}
